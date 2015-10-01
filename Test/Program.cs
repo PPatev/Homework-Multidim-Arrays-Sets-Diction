72a4182e10767931251d5557d124418d06f2fbdb
Peter Patev <petar1patev@gmail.com>
2015-10-01 13:55:50 +0300

    Added comments in \Test\Program.cs

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,20 +1,37 @@
 ﻿using System;
+
 using System.Collections.Generic;
+
 using System.Linq;
 using System.Text;
+
 using System.Threading.Tasks;
 
+
 namespace Test
+
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string command = "Rotate(270)";
-            int from = command.IndexOf("(") + "(".Length;
-            int to = command.LastIndexOf(")");
-            string comm = command.Substring(from, to - from);
-            Console.WriteLine(comm);
-        }
-    }
+    
+	class Program
+    
+	{
+        
+		static void Main(string[] args)
+
+	        {
+       string command = "Rotate(270)";
+
+            		int from = command.IndexOf("(") + "(".Length;
+
+           	        int to = command.LastIndexOf(")");
+
+            		string comm = command.Substring(from, to - from);
+
+                	Console.WriteLine(comm);
+
+			//This is just a test. "Test" is not a working program.
+        	}
+    
+	}
+
 }
